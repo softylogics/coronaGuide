644a0f4e4791658e837470a20c43dfeb23250579
Rehan Sid <softylogics@gmail.com>
2020-04-16 19:58:33 +0500

    ALmost complete

--- app/src/main/java/com/example/coronaguide/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/coronaguide/ui/gallery/GalleryFragment.java
@@ -22,11 +22,11 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.mask_guidelines, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
